4fc975843bd72408adccfcb751c51d54ebb5ba20
E Simonds <esimonds09@gmail.com>
2022-05-25 00:48:10 +0200

    refactor(flash-card): clean code

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <!-- <v-card class="logo py-4 d-flex justify-center"> -->
+
         <TheCard/>
-      <!-- </v-card> -->
+
     </v-col>
   </v-row>
 </template>
